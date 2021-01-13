a09b87475bdeec3a6553388dc9a94507d745812e
MariaZyryanova72 <mariyazz@inbox.ru>
2021-01-13 12:46:26 +0500

    Линейная регрессия

---
+++ 4_lesson/main_1.py
@@ -0,0 +1,77 @@
+# Библиотека SKLearn - библиотека для решения задач машинного обучения.
+import sklearn
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+import matplotlib.pyplot as plt
+
+# linear_model и neighbors
+
+
+pd.set_option('display.max_rows', 100, 'display.max_columns', 200)
+# строчка нужна, чтобы датасеты с множеством колонок отображались полностью
+
+
+#########################
+# Линейная регрессия
+#########################
+
+# Линейная регрессия (англ. Linear regression) — используемая в статистике регрессионная модель
+# зависимости одной (объясняемой, зависимой) переменной. от другой или нескольких других переменных
+# (факторов, регрессоров, независимых переменных). с линейной функцией зависимости.
+
+noise = np.random.randn(20) * 10
+print(noise)
+
+x = np.linspace(-5, 5, 20)
+y = 10 * x - 7 + noise
+
+plt.scatter(x, y)
+plt.show()
+
+# Разобьем данные на тренировочный и тестовый набор
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
+
+# Давайте посмотрим на данные
+plt.figure(figsize=(10, 5))
+plt.plot(x, 10 * x - 7, label='real')
+plt.scatter(x_train, y_train, label='train')
+plt.scatter(x_test, y_test, label='test')
+plt.legend()
+plt.show()
+
+# Давайте создадим модель и обучим ее. Но перед этим, нам надо немного преобразовать данные.
+# На вход модели мы должны подавать матрицу, а сейчас мы имеем вектор.
+
+print(x_train.shape)
+print(x_test.shape)
+x_train = x_train.reshape(-1, 1)
+x_test = x_test.reshape(-1, 1)
+print(x_train.shape)
+print(x_test.shape)
+
+print(x_train)
+print(x_test)
+
+model = LinearRegression()
+model.fit(x_train, y_train)
+# Теперь мы можем сделать предикт.
+predictions = model.predict(x_test)
+print(predictions)
+
+# Так же можем посмотреть коэффеценты.
+k = model.coef_
+b = model.intercept_
+print(k, b)
+
+plt.figure(figsize=(10, 5))
+plt.plot(x, 10 * x - 7, label='real')
+plt.scatter(x_train, y_train, label='train')
+plt.scatter(x_test, y_test, label='test')
+plt.plot(x, x * k + b, label='predicted')
+plt.legend()
+plt.show()
