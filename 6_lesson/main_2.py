a0fcc97e5c7149999c05411b6f9e44156395b58f
MariaZyryanova72 <mariyazz@inbox.ru>
2021-01-30 14:34:04 +0500

    Классификация

--- 6_lesson/main_2.py
+++ 6_lesson/main_2.py
@@ -9,6 +9,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 from tensorflow.keras.utils import plot_model
 
@@ -35,3 +36,33 @@ model.compile(loss='binary_crossentropy',
 model.summary()
 
 model.fit(data, y, batch_size=100, epochs=20, validation_split=0.3)
+
+
+################
+# Классификация
+################
+
+# В Tensorflow есть готовые датасеты для обучения.
+# Одним из таких датасетов является датасет с изодражением рукописных цифр
+
+mnist = tf.keras.datasets.mnist
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+print(train_images.shape)
+print(train_labels.shape)
+print(test_images.shape)
+print(test_labels.shape)
+
+plt.colorbar(plt.imshow(train_images[0], cmap='binary_r'))
+plt.show()
+print(train_labels[0])
+
+
+from tensorflow.keras.utils import to_categorical
+
+train_labels_categorical = to_categorical(train_labels, 10)
+test_labels_categorical = to_categorical(test_labels, 10)
+print(train_labels.shape)
+print(train_labels_categorical.shape)
+
+print(train_labels_categorical[0])
